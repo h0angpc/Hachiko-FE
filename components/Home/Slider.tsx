721ded7ca54b41a750a4ef63e820028b79a2ed73
h0angpc <118001661+h0angpc@users.noreply.github.com>
2025-02-28 00:04:37 +0700

    add header for homescreen and 7 image for slider

---
+++ components/Home/Slider.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from 'react'
+
+export default function Slider() {
+    return (
+        <View>
+            <Text>Slider 2</Text>
+        </View>
+    )
+}
